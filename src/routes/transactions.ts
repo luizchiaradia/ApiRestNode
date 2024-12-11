ec413e22ac7e091e80e13cb4dafb3eef4fc2b660
Luiz Chiaradia <luizexchiaradia@gmail.com>
2024-12-11 11:50:47 -0300

    up

---
+++ src/routes/transactions.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+
+export async function transactionsRoutes(app:FastifyInstance) {
+  app.get("/hello", async () => {
+    const tables = await knex("transactions").select("*");
+    return tables;
+  });
+}
