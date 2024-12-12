e876e6dc96f1d6f0e81a0f1a6fc28247dce655c7
Luiz Chiaradia <luizexchiaradia@gmail.com>
2024-12-12 09:48:45 -0300

    up

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,25 @@ import { knex } from "../database";
 import { randomUUID } from "crypto";
 
 export async function transactionsRoutes(app:FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select();
+    return { transactions };
+  });
+
+  app.get("/summary", async () => {
+    const summary = await knex("transactions").sum("amount", { as: "amount" })
+    return { summary };
+  });
+
+  app.get("/:id", async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+    const { id } = getTransactionParamsSchema.parse(request.params);
+    const transaction = await knex("transactions").where({ id }).first();
+    return { transaction };
+  });
+
   app.post("/", async (request, reply) => {
     const creatTrasnsactionBodySchema = z.object({
       title: z.string(),
