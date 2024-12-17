ce6862e9ae2be4ce26db53a387a27bbbe252d4de
Luiz Chiaradia <luizexchiaradia@gmail.com>
2024-12-17 12:53:11 -0300

    up

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   DATABASE_CLIENT: z.enum(['pg', 'sqlite']).default('pg'),
-  PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.number().default(10000),
   DATABASE_URL: z.string(),
 });
 
